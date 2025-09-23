374ce9caf84aa94439914e497c0f666383c807e1
ahiggins1-dev <ahiggins1@mail.bradley.edu>
2025-09-22 20:50:05 -0500

    Title Screen Functioning!
    yay

--- Assets/Scripts/SceneMoving/StartGame.cs
+++ Assets/Scripts/SceneMoving/StartGame.cs
@@ -1,12 +1,36 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void LevelMove()
-    {
-        //SceneManager.
-    }
 
+    [SerializeField] private GameObject button;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void LevelMove()
+    {
+        if(button.tag == "1Player")
+        {
+            GameInformation.NumPlayers = 1;
+            SceneManager.LoadSceneAsync(1);
+        }
+
+        else
+        {
+            GameInformation.NumPlayers = 2;
+            SceneManager.LoadSceneAsync(1);
+        }
+
+        if(GameInformation.NumPlayers % 2 != 0)
+        {
+            Debug.Log("Loading up a game with 1 player!");
+        }
+        else
+        {
+            Debug.Log("Loading up a game with 2 players!");
+        }
+        
+    }
 }
