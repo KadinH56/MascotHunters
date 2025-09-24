f78076e6fe9b687aa6b7bc236ea05488439d3e25
LucasFehlberg <lfehlberg@mail.bradley.edu>
2025-09-24 12:45:57 -0500

    Working Enemies

--- Assets/Scripts/Player/PlayerStatManager.cs
+++ Assets/Scripts/Player/PlayerStatManager.cs
@@ -8,4 +8,28 @@ public class PlayerStatManager : MonoBehaviour
     /// Used to modify player stats or utilize the values
     /// </summary>
     public Stats PlayerStats { get => playerStats; set => playerStats = value; }
+
+    private void Start()
+    {
+        OnAlive();
+    }
+    /// <summary>
+    /// Take damage
+    /// </summary>
+    /// <param name="damage">Amount of damage</param>
+    public void TakeDamage(int damage)
+    {
+        playerStats.Health -= damage;
+
+        if(playerStats.Health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnAlive()
+    {
+        gameObject.SetActive(true);
+        playerStats.Health = playerStats.MaxHealth;
+    }
 }
