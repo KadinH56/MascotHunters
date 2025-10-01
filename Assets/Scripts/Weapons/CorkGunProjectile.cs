5d3df5a34c8a3a5e7ac9cbfd9d7d0e7d6efbde35
LucasFehlberg <lfehlberg@mail.bradley.edu>
2025-10-01 12:47:35 -0500

    Start explosive

--- Assets/Scripts/Weapons/CorkGunProjectile.cs
+++ Assets/Scripts/Weapons/CorkGunProjectile.cs
@@ -1,6 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class CorkGunProjectile : Projectile
 {
-    
+    [SerializeField] private float explosionDamageMultiplier;
+
+    [SerializeField] private GameObject[] sprites;
+    [SerializeField] private Collider projectileCollider;
+    [SerializeField] private Collider explosionCollider;
+
+    public override void OnKill(bool hitTarget)
+    {
+        if (hitTarget)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Damage = Mathf.RoundToInt(Damage * explosionDamageMultiplier);
+        foreach (GameObject sprite in sprites)
+        {
+            sprite.SetActive(false);
+        }
+        projectileCollider.enabled = false;
+        explosionCollider.enabled = true;
+
+        StartCoroutine(DestroyMe());
+    }
+
+    private IEnumerator DestroyMe()
+    {
+        yield return new WaitForFixedUpdate();
+        Destroy(gameObject);
+    }
 }
