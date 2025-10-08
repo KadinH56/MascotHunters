8d9e6d75bc85707eee2433c5e228882396540fe7
LucasFehlberg <lfehlberg@mail.bradley.edu>
2025-10-08 16:36:14 -0500

    Misc stuff that is and isnt related to this branch

--- Assets/Scripts/Weapons/LadderRotate.cs
+++ Assets/Scripts/Weapons/LadderRotate.cs
@@ -5,6 +5,15 @@ public class LadderRotate : WeaponBase
     [SerializeField] private float rotationSpeed;
     [SerializeField] private bool isRotating;
 
+    [SerializeField] private GameObject secondLadderObject;
+    [SerializeField] private Collider secondLadderCollider;
+
+    private void Start()
+    {
+        LevelupDescriptions.Add(1, "Get a Spinning Ladder");
+        LevelupDescriptions.Add(2, "Add a Second Ladder");
+    }
+
     /// <summary>
     /// If the bool is true, the ladder will rotate around the fixed object
     /// </summary>
@@ -21,6 +30,19 @@ public class LadderRotate : WeaponBase
         if(other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.GetComponent<EnemyScript>().TakeDamage(CalculateDamage());   
+        }
+    }
+
+    public override void LevelUpWeapon()
+    {
+        weaponLevel++;
+        switch (weaponLevel)
+        {
+            case 2:
+                //speedModifier = 0.9f;
+                secondLadderCollider.enabled = true;
+                secondLadderObject.SetActive(true);
+                break;
         }
     }
 }
