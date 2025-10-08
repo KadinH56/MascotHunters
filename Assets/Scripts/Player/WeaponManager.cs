277946ade0f138905515325ac0d949995dfeb160
LucasFehlberg <lfehlberg@mail.bradley.edu>
2025-10-08 12:04:33 -0500

    Committing player changes

---
+++ Assets/Scripts/Player/WeaponManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    private string[] weapons = new string[2];
+
+    public string[] Weapons { get => weapons; set => weapons = value; }
+
+    public void WeaponUpgrade(string newWeapon)
+    {
+        if (weapons[0] == newWeapon || weapons[1] == newWeapon)
+        {
+            transform.Find(newWeapon).GetComponent<WeaponBase>().LevelUpWeapon();
+            return;
+        }
+
+        if (weapons[0] == null)
+        {
+            weapons[0] = newWeapon;
+            transform.Find(newWeapon).gameObject.SetActive(true);
+            return;
+        }
+
+        if (weapons[1] == null)
+        {
+            weapons[1] = newWeapon;
+            transform.Find(newWeapon).gameObject.SetActive(true);
+            return;
+        }
+    }
+
+    public WeaponStat[] GetWeapons()
+    {
+        WeaponStat[] stats = new WeaponStat[2];
+
+        for(int i = 0; i < weapons.Length; i++)
+        {
+            if(weapons[i] == null)
+            {
+                stats[i] = null;
+                continue;
+            }
+
+            WeaponStat stat = new();
+            stat.Weapon = weapons[i];
+            stat.Level = transform.Find(weapons[i]).GetComponent<WeaponBase>().WeaponLevel;
+        }
+
+        return stats;
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponStat.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class WeaponStat
+{
+    private string weapon;
+    private int level;
+
+    public string Weapon { get => weapon; set => weapon = value; }
+    public int Level { get => level; set => level = value; }
+}
