9dc77e40228ec67e96025d38c009b22b37f8f134
LucasFehlberg <lfehlberg@mail.bradley.edu>
2025-11-17 10:59:39 -0600

    Arcade PC UI

--- Assets/Scripts/UI/UIBuildTextureManager.cs
+++ Assets/Scripts/UI/UIBuildTextureManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 [Serializable]
 public class UIBuildTextureManager
@@ -14,4 +15,52 @@ public class UIBuildTextureManager
     /// If we have a spriteRenderer, use this
     /// </summary>
     [SerializeField] private SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// If we have an Image, use this
+    /// </summary>
+    [SerializeField] private Image image;
+
+    /// <summary>
+    /// If for whatever reason we have a rawImage...you get the idea
+    /// </summary>
+    [SerializeField] private RawImage rawImage;
+
+    public UIBuildTextureManager()
+    {
+        TextureManagers.Add(this);
+    }
+
+    ~UIBuildTextureManager()
+    {
+        TextureManagers.Remove(this);
+    }
+
+    public void SetTexture()
+    {
+        Sprite sprite = GameInformation.IsArcadeBuild ? arcadeBuild : PCBuild;
+
+        if (image != null)
+        {
+            image.sprite = sprite;
+        }
+
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = sprite;
+        }
+
+        if (rawImage != null)
+        {
+            rawImage.texture = sprite.texture;
+        }
+    }
+
+    public static void SetAllTextures()
+    {
+        foreach (UIBuildTextureManager textureManager in TextureManagers)
+        {
+            textureManager.SetTexture();
+        }
+    }
 }
