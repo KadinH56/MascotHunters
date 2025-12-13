ea5ed3893e3588da25ccc9724103fba8656effd3
LucasFehlberg <lfehlberg@mail.bradley.edu>
2025-12-13 15:25:57 -0600

    Finish this because I forgot

--- Assets/Scripts/Player/StartP1P2.cs
+++ Assets/Scripts/Player/StartP1P2.cs
@@ -14,6 +14,7 @@ public class ResetQuit : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
+        Cursor.visible = false;
         pInput = GetComponent<PlayerInput>();
 
         InputDevice[] devices = new InputDevice[1];
